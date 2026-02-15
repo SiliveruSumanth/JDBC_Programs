89479f84da5968e97eabcb4643e15b7ce4ec87c9
Siliveru Sumanth <siliverusumanth7@gmail.com>
2026-02-15 22:42:43 +0530

    Add files via upload

---
+++ R-Read/DESC/src/ReadData.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+public class ReadData {
+    static String url="jdbc:mysql://localhost:3306/JDBC_1";
+    static String user="root";
+    static String pin="0053";
+    static Connection con;
+    public static void main(String[] args){
+        try {
+            con=DriverManager.getConnection(url, user, pin);
+            Statement st=con.createStatement();
+            String sql="desc users";
+            ResultSet rs=st.executeQuery(sql);
+            while(rs.next()){
+              System.out.println(
+                rs.getString("Field")
+                +" : "+
+                rs.getString("Type"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
